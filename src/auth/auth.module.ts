1d8d86bac35bb96507f20c18cad6170048c5ebed
Sai Chaitanya Raparti <chaitanya.raparti@gmail.com>
2024-12-25 18:33:12 +0530

    Databse Integration 1. Renamed to UsersModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { UserModule } from 'src/users/users.module';
+import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { JwtAuthGuard, LocalAuthGuard } from './auth.guard';
 import { BaseAuthService, JwtAuthService } from './auth.service';
@@ -19,7 +19,7 @@ import { LocalStrategy } from './local.strategy';
   ],
   controllers: [AuthController],
   imports: [
-    UserModule,
+    UsersModule,
     PassportModule,
     JwtModule.register({
       global: true,
