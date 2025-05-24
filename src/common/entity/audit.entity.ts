edbe9ca22574b963f2cc3dc7834e5a6d7b882487
Sai Chaitanya Raparti <chaitanya.raparti@gmail.com>
2025-05-24 15:54:39 +0530

    Add soft delete functionality to AuditEntity by including deletedAt column

--- src/common/entity/audit.entity.ts
+++ src/common/entity/audit.entity.ts
@@ -1,9 +1,18 @@
-import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { CreateDateColumn, DeleteDateColumn, UpdateDateColumn } from 'typeorm';
 
 export abstract class AuditEntity {
-  @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
-  createdAt: Date;
+    @CreateDateColumn({
+        type: 'timestamptz',
+        default: () => 'CURRENT_TIMESTAMP',
+    })
+    createdAt: Date;
 
-  @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
-  updatedAt: Date;
+    @UpdateDateColumn({
+        type: 'timestamptz',
+        default: () => 'CURRENT_TIMESTAMP',
+    })
+    updatedAt: Date;
+
+    @DeleteDateColumn({ type: 'timestamptz', nullable: true })
+    deletedAt: Date | null; // Soft delete column
 }
