ce28137fef1dd5407506992b8ce4ed9d49f7edce
Sai Chaitanya Raparti <chaitanya.raparti@gmail.com>
2025-05-25 13:08:32 +0530

    Enhance applicant entity and services: refine password exclusion, add conflict handling in applicant creation, and update registration logic to use CreateApplicantDto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,16 @@
 import {
-  Body,
-  Controller,
-  Logger,
-  Post,
-  Request,
-  UseGuards,
+    Body,
+    Controller,
+    Logger,
+    Post,
+    Request,
+    UseGuards,
 } from '@nestjs/common';
 import { ApiBody } from '@nestjs/swagger';
 import {
-  CreateApplicantDto,
-  LoginApplicantDTO,
+    CreateApplicantDto,
+    LoginApplicantDTO,
 } from '../applicants/applicant.dto';
-import { Applicant } from '../applicants/applicant.entity';
 import { LocalAuthGuard } from './auth.guard';
 import { BaseAuthService } from './auth.service';
 import { Public } from './public.decorator';
@@ -19,21 +18,21 @@ import { Public } from './public.decorator';
 @Controller('auth')
 @Public()
 export class AuthController {
-  private readonly logger = new Logger(AuthController.name);
-  constructor(private authService: BaseAuthService) {}
+    private readonly logger = new Logger(AuthController.name);
+    constructor(private authService: BaseAuthService) {}
 
-  @UseGuards(LocalAuthGuard)
-  @Post('login')
-  @ApiBody({ type: LoginApplicantDTO })
-  async login(@Request() req): Promise<{ access_token: string }> {
-    this.logger.log(`Login request for User: ${req.user.username}`);
-    return await this.authService.login(req.user);
-  }
+    @UseGuards(LocalAuthGuard)
+    @Post('login')
+    @ApiBody({ type: LoginApplicantDTO })
+    async login(@Request() req): Promise<{ access_token: string }> {
+        this.logger.log(`Login request for User: ${req.user.username}`);
+        return await this.authService.login(req.user);
+    }
 
-  @Post('register')
-  @ApiBody({ type: CreateApplicantDto })
-  async register(@Body() user: Applicant) {
-    this.logger.log(`Register request for User: ${user.username}`);
-    await this.authService.register(user);
-  }
+    @Post('register')
+    @ApiBody({ type: CreateApplicantDto })
+    async register(@Body() user: CreateApplicantDto) {
+        this.logger.log(`Register request for User: ${user.username}`);
+        await this.authService.register(user);
+    }
 }
