1c77293b88242ec35aa1c4d9cbd18fcad0130777
Om Bhor <ombhor617@gmail.com>
2025-06-29 20:45:21 +0530

    add static method to create Job entity from application DTO

--- src/jobs/entities/job.entity.ts
+++ src/jobs/entities/job.entity.ts
@@ -1,6 +1,7 @@
 import { Column, Entity } from 'typeorm';
 import { AuditEntity } from '../../common/entity/audit.entity';
 import { JobStatus } from '../job-status.enum';
+import { CreateApplicationDto } from 'src/applications/dto/create-application.dto';
 
 @Entity('jobs')
 export class Job extends AuditEntity {
@@ -41,4 +42,20 @@ export class Job extends AuditEntity {
 
     @Column()
     recruiterId: number;
+
+    
+    static fromApplication(application: CreateApplicationDto): Job {
+        let jobEntity = new Job();
+        jobEntity.role = application.role;
+        jobEntity.company = application.company;
+        jobEntity.jobLink = application.jobLink;
+        jobEntity.city = application.city;
+        jobEntity.state = application.state;
+        jobEntity.country = application.country;
+        jobEntity.description = application.description;
+        jobEntity.ctcOffered = application.ctcOffered;
+        jobEntity.status = JobStatus.Open;
+
+        return jobEntity;
+    }
 }
